c6deb31da76d8f241a7a618b94c955577862395a
+ <+dannaewaltz@gmail.com>
2022-06-20 23:20:01 -0600

    commit tree 4cac860d24905223b94fcd8b5b630f987c2082c4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"]
+
+
+function findMatching(drivers, Bobby) {
+    return drivers.filter(
+        (driversMatch) => driversMatch.toLowerCase() === Bobby.toLowerCase());
+}
+
+function fuzzyMatch(drivers, Sa) {
+    return drivers.filter(
+        (matchDriver) => matchDriver.toLowerCase().indexOf(Sa.toLowerCase()) === 0
+    );
+}
+
+function matchName(drivers, nameFound) {
+    return drivers.filter((driver) => driver.name === nameFound);
+}
+ 
